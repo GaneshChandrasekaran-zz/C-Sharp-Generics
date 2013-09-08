79907e49538ba93edef23da4536ff43713793b58
Ganesh Chandrasekaran <gxc4795@rit.edu>
2013-09-08 16:36:08 -0400

    Enumerator and testing left
    All working fine so far.
    
    Implement Enumerator.
    Write Test Cases.

--- Solution/Solution/Solution.cs
+++ Solution/Solution/Solution.cs
@@ -43,7 +43,7 @@ namespace Solution
     /// </summary>
     /// <param name="Key">the types of the table's keys (uses Equals())</param>
     /// <param name="Value">the types of the table's values</param>
-    interface Table<Key, Value>
+    public interface Table<Key, Value>
     {
         /// <summary>
         /// Add a new entry in the hash table. If an entry with the
@@ -102,7 +102,9 @@ namespace Solution
     class LinkedHashTable<Key, Value> : Table<Key, Value>
     {
         int sizeOfArray;
-        double threshold;
+        double definedThreshold;
+
+        int countOfElements = 0;
 
         public static List<Node<Key, Value>>[] arrayList;
 
@@ -115,7 +117,7 @@ namespace Solution
         /// reached will cause a rebuild of the table to a 50% larger size</param>
         public LinkedHashTable(Int32 size, Double threshold)
         {
-            this.threshold = threshold;
+            this.definedThreshold = threshold;
             this.sizeOfArray = size;
 
             arrayList = new List<Node<Key, Value>>[sizeOfArray];
@@ -128,7 +130,7 @@ namespace Solution
         /// </summary>
         /// <param name="k">The key for the new or existing entry</param>
         /// <param name="v">The (new) value for the key</param>
-        void Put(Key k, Value v)
+        public void Put(Key k, Value v)
         {
             int location = HashFunction(k);
 
@@ -143,10 +145,20 @@ namespace Solution
                     List<Node<Key, Value>> fillerList = new List<Node<Key, Value>>();
                     arrayList[location] = fillerList;
                     arrayList[location].Add(newNode);
+                    countOfElements++;
+                    if (exceedsThreshold())
+                    {
+                        resizeTable();
+                    }
                 }
                 else
                 {
                     arrayList[location].Add(newNode);
+                    countOfElements++;
+                    if (exceedsThreshold())
+                    {
+                        resizeTable();
+                    }
                 }
             }
             // If a key already exists, then its value is updated with
@@ -181,7 +193,7 @@ namespace Solution
         /// </summary>
         /// <param name="k">The key being sought</param>
         /// <returns>true if and only if the key exists in the table</returns>
-        bool Contains(Key k)
+        public bool Contains(Key k)
         {
             int location = HashFunction(k);
 
@@ -201,14 +213,13 @@ namespace Solution
             return false;
         }
 
-
         /// <summary>
         /// Fetch the value associated with the given key.
         /// </summary>
         /// <param name="k">The key to be looked up in the table</param>
         /// <returns>the value associated with the given key</returns>
         /// <exception cref="NonExistentKey">if Contains(key) is false</exception>
-        Value Get(Key k)
+        public Value Get(Key k)
         {
             int location = HashFunction(k);
 
@@ -247,6 +258,91 @@ namespace Solution
             }
             return nodeValue;
         }
+
+
+        bool exceedsThreshold()
+        {
+            double getCount = countOfElements;
+            double getSize = sizeOfArray;
+            double currentThreshold = getCount / getSize;
+            if (currentThreshold >= definedThreshold)
+                return true;
+            else
+                return false;
+        }
+
+        public static List<Node<Key, Value>>[] newArrayList;
+
+        void resizeTable()
+        {
+            double computeNewSize = (sizeOfArray + (sizeOfArray / 2));
+            int newSize = (int)(computeNewSize);
+
+            this.sizeOfArray = newSize;
+
+            newArrayList = new List<Node<Key, Value>>[sizeOfArray];
+
+            for (int i = 0; i < arrayList.Length; i++)
+            {
+                rehashTable(i);
+            }
+
+            arrayList = newArrayList;
+
+            Console.WriteLine("Successfully rehashed");
+        }
+
+        void rehashTable(int i)
+        {
+            List<Node<Key, Value>> myList = arrayList[i];
+
+            if (myList == null)
+                return;
+
+            foreach (Node<Key, Value> node in myList)
+            {
+                Key k;
+                Value v;
+
+                if (node == null)
+                {
+                    Console.Write("");
+                }
+                else
+                {
+                    k = node.key;
+                    v = node.value;
+                    rePut(k, v);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add the existing entry in the new hash table.
+        /// rePut() always succeeds.
+        /// </summary>
+        /// <param name="k">The key for the new or existing entry</param>
+        /// <param name="v">The (new) value for the key</param>
+        void rePut(Key k, Value v)
+        {
+            int location = HashFunction(k);
+
+            // Creates a new node to be added in the data structure.
+            Node<Key, Value> newNode = new Node<Key, Value>(k, v);
+
+            if (newArrayList[location] == null)
+            {
+                List<Node<Key, Value>> fillerList = new List<Node<Key, Value>>();
+                newArrayList[location] = fillerList;
+                newArrayList[location].Add(newNode);
+            }
+            else
+            {
+                newArrayList[location].Add(newNode);
+            }
+        }
+
+
     }
 
     //class TestTable
@@ -272,72 +368,54 @@ namespace Solution
         /// reached will cause a rebuild of the table to a 50% larger size
         /// </param>
         /// <returns>A new instance of Table</returns>
-        public static Table<K, V> Make<K, V>(int capacity = 100, double loadThreshold = 0.75)
+        public static Table<K, V> Make<K, V>(int capacity, double loadThreshold)
         {
             return (new LinkedHashTable<K, V>(capacity, loadThreshold));
         }
 
     }
+
     class MainClass
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
-            Table<String, String> ht = TableFactory.Make<String, String>(100, 0.5);
-            ht.Put("1", "Hi");
-            ht.Put("1", "Hello");
-            ht.Put("2", "Height");
-            ht.Put("3", "Lolwa");
-            ht.Put("2", "HelloKitty");
+            Table<String, String> ht = TableFactory.Make<String, String>(4, 0.5);
+            ht.Put("Joe", "Doe");
+            Console.WriteLine(ht.Get("Joe"));
+            ht.Put("Jane", "Brain");
+            Console.WriteLine(ht.Get("Jane"));
+            ht.Put("Chris", "Swiss");
+            Console.WriteLine(ht.Get("Chris"));
+
             try
             {
-                Console.WriteLine(ht.Get("1"));
-                Console.WriteLine(ht.Get("5"));
+                //foreach (String first in ht)
+                //{
+                //    Console.WriteLine(first + " -> " + ht.Get(first));
+                //}
+                Console.WriteLine("=========================");
+
+                ht.Put("Wavy", "Gravy");
+                Console.WriteLine(ht.Get("Wavy"));
+                ht.Put("Chris", "Bliss");
+                Console.WriteLine(ht.Get("Chris"));
+                //foreach (String first in ht)
+                //{
+                //    Console.WriteLine(first + " -> " + ht.Get(first));
+                //}
+                Console.WriteLine("=========================");
+
+                Console.Write("Jane -> ");
+                Console.WriteLine(ht.Get("Jane"));
+                Console.Write("John -> ");
+                Console.WriteLine(ht.Get("John"));
             }
             catch (NonExistentKey<String> nek)
             {
                 Console.WriteLine(nek.Message);
                 Console.WriteLine(nek.StackTrace);
             }
+            Console.ReadLine();
         }
     }
-
 }
-
-
-//class MainClass
-//{
-//    public static void Main(string[] args)
-//    {
-//        Table<String, String> ht = TableFactory.Make<String, String>(4, 0.5);
-//        ht.Put("Joe", "Doe");
-//        ht.Put("Jane", "Brain");
-//        ht.Put("Chris", "Swiss");
-//        try
-//        {
-//            foreach (String first in ht)
-//            {
-//                Console.WriteLine(first + " -> " + ht.Get(first));
-//            }
-//            Console.WriteLine("=========================");
-
-//            ht.Put("Wavy", "Gravy");
-//            ht.Put("Chris", "Bliss");
-//            foreach (String first in ht)
-//            {
-//                Console.WriteLine(first + " -> " + ht.Get(first));
-//            }
-//            Console.WriteLine("=========================");
-
-//            Console.Write("Jane -> ");
-//            Console.WriteLine(ht.Get("Jane"));
-//            Console.Write("John -> ");
-//            Console.WriteLine(ht.Get("John"));
-//        }
-//        catch (NonExistentKey<String> nek)
-//        {
-//            Console.WriteLine(nek.Message);
-//            Console.WriteLine(nek.StackTrace);
-//        }
-//        Console.ReadLine();
-//    }
-//}
